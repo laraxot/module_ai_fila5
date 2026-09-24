9a7c5bdb6947bdc4349c7922e79916f8eb4ef55f
Marco Xot <marco.sottana@gmail.com>
2026-09-24 16:12:06 +0000

    .

--- app/Filament/Resources/AiActionProposalResource/Pages/ViewAiActionProposal.php
+++ app/Filament/Resources/AiActionProposalResource/Pages/ViewAiActionProposal.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Modules\AI\Filament\Resources\AiActionProposalResource\Pages;
 
+use Filament\Schemas\Components\Component;
 use Modules\AI\Filament\Resources\AiActionProposalResource;
 use Modules\AI\Filament\Resources\AiActionProposalResource\Schemas\AiActionProposalInfolist;
 use Modules\Xot\Filament\Resources\Pages\XotBaseViewRecord;
@@ -16,6 +17,8 @@ class ViewAiActionProposal extends XotBaseViewRecord
     /**
      * @return array<string, \Filament\Schemas\Components\Component>
      */
+    #[\Override]
+    protected function getInfolistSchema(): array
     {
         return app(AiActionProposalInfolist::class)->getInfolistSchema();
     }
