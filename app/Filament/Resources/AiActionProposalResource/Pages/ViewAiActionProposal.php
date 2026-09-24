a8245c44ad8d50a4da0f450d59d718a87ba0507f
Marco Xot <marco.sottana@gmail.com>
2026-09-24 10:42:01 +0000

    Remove phpinsights.php configuration file and various archived documentation files to declutter the project and improve maintainability.

--- app/Filament/Resources/AiActionProposalResource/Pages/ViewAiActionProposal.php
+++ app/Filament/Resources/AiActionProposalResource/Pages/ViewAiActionProposal.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Modules\AI\Filament\Resources\AiActionProposalResource\Pages;
 
-use Filament\Schemas\Components\Component;
 use Modules\AI\Filament\Resources\AiActionProposalResource;
 use Modules\AI\Filament\Resources\AiActionProposalResource\Schemas\AiActionProposalInfolist;
 use Modules\Xot\Filament\Resources\Pages\XotBaseViewRecord;
@@ -13,5 +12,13 @@ class ViewAiActionProposal extends XotBaseViewRecord
 {
     protected static string $resource = AiActionProposalResource::class;
 
-    
+
+    /**
+     * @return array<string, \Filament\Schemas\Components\Component>
+     */
+    #[\Override]
+    protected function getInfolistSchema(): array
+    {
+        return app(AiActionProposalInfolist::class)->getInfolistSchema();
+    }
 }
